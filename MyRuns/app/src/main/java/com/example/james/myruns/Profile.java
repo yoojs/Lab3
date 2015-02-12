c9d0eb45c6e0ca1a6856eb9a387d8d5f34a6183b
yoojs <yoojs@dukes.jmu.edu>
2015-02-12 17:43:20 -0500

    SharedPreferences done

--- MyRuns/app/src/main/java/com/example/james/myruns/Profile.java
+++ MyRuns/app/src/main/java/com/example/james/myruns/Profile.java
@@ -1,19 +1,76 @@
 package com.example.james.myruns;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 
 public class Profile extends ActionBarActivity {
-
+    public static final String PREFS_NAME = "com.example.james.myruns";
+    EditText name, email, phone, classes, major;
+    RadioGroup radioGenders;
+    int gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        name = (EditText) findViewById(R.id.editName);
+        email = (EditText) findViewById(R.id.editEmail);
+        phone = (EditText) findViewById(R.id.editPhone);
+        classes = (EditText) findViewById(R.id.editClass);
+        major = (EditText) findViewById(R.id.editMajor);
+        RadioGroup radioGenders = (RadioGroup) findViewById(R.id.radioGender);
+
+        radioGenders.check(settings.getInt("gender", -1));
+
+        name.setText(settings.getString("name",""));
+        email.setText(settings.getString("major",""));
+        phone.setText(settings.getString("phone",""));
+        classes.setText(settings.getString("class",""));
+        major.setText(settings.getString("major",""));
+
     }
 
+    public void savePreferences(View view) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        name = (EditText) findViewById(R.id.editName);
+        email = (EditText) findViewById(R.id.editEmail);
+        phone = (EditText) findViewById(R.id.editPhone);
+        classes = (EditText) findViewById(R.id.editClass);
+        major = (EditText) findViewById(R.id.editMajor);
+        radioGenders = (RadioGroup) findViewById(R.id.radioGender);
+
+        int selectedId = radioGenders.getCheckedRadioButtonId();
+        gender = radioGenders.indexOfChild(findViewById(selectedId));
+
+
+
+        String savedName = name.getText().toString();
+        String savedEmail= email.getText().toString();
+        String savedPhone = phone.getText().toString();
+        String savedClass = classes.getText().toString();
+        String savedMajor = major.getText().toString();
+
+        editor.putInt("gender", gender);
+        editor.putString("name", savedName);
+        editor.putString("email", savedEmail);
+        editor.putString("phone", savedPhone);
+        editor.putString("class", savedClass);
+        editor.putString("major", savedMajor);
+
+        editor.commit();
+
+    }
+    public void cancelActivity(View view) {
+        finish();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +93,7 @@ public class Profile extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
